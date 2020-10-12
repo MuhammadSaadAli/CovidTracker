94723d2f4ce10519202fa74f793b4a04fac0471f
MuhammadSaadAli <saadalikhan96@gmail.com>
2020-10-12 14:51:08 +0500

    Updates

---
+++ precache-manifest.316e438675e7e8ad750bd79499cc63c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2d3ca90673194ddc103bcfa1f6320f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92b9badfd9d357d25db9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "67851f6704a46a184e14",
+    "url": "/static/js/2.d0ba47d9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d0ba47d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92b9badfd9d357d25db9",
+    "url": "/static/js/main.d025e36c.chunk.js"
+  },
+  {
+    "revision": "4df097abbea25f86dac0",
+    "url": "/static/js/runtime-main.f10a8730.js"
+  }
+]);
